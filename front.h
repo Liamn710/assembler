d72d5e85dfcadbabee4daef37d9d3d45e9bad467
Liam <liamn7101996@gmail.com>
2024-08-10 13:57:48 +0300

    added errors, more validation functions and utils

--- front.h
+++ front.h
@@ -1,4 +1,20 @@
 struct opcode {
     char * name;
     int op_num;
+    int arg_num;
 };
+/* Represents different parts of a data line */
+typedef struct inst_parts {
+    char *label;       /* The label associated with the instruction */
+    short *nums;       /* An array of short integers representing instruction data */
+    int len;           /* The number of data in *nums including '0' at the end of a string */
+    char *arg_label;   /* The label associated with an argument, if applicable */
+    int is_extern;     /* Indicates whether the instruction is marked as external */
+} inst_parts;
+
+typedef struct command_parts {
+    char *label;     /* The label associated with the command */
+    int opcode;      /* The opcode of the command */
+    char *source;    /* The source operand of the command */
+    char *dest;      /* The destination operand of the command */
+} command_parts;
